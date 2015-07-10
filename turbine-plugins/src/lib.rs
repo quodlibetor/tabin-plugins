818b0afcadd16377829db0d3aee476104e8516d3
Brandon W Maister <bwm@knewton.com>
2015-07-10 11:43:28 -0500

    turbine-plugin: docs

--- turbine-plugins/src/lib.rs
+++ turbine-plugins/src/lib.rs
@@ -1,17 +1,38 @@
+//! Utitilty for writing nagios-style check scripts/plugins
+//!
+//! Right now this is just the `ExitStatus` struct, useful for making sure that
+//! you exit correctly.
+
+
 use std::process;
 use std::cmp::{Ord};
 
+/// All errors are TurbineErrors
+#[derive(Debug)]
+pub enum TurbineError {
+    /// Represents an incorrect value passed in to a function
+    UnknownValue(String)
+}
+
+/// All results are TurbineResults
+pub type TurbineResult<T> = Result<T, TurbineError>;
+
+/// Represent the nagios-ish error status of a script.
 #[must_use]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ExitStatus {
+    /// Unexpected result
     Unknown,
+    /// Everything is fine
     Ok,
+    /// Something is weird, but don't necessary call anybody
     Warning,
+    /// OMG CALL SOMEONE I DON'T CARE WHAT TIME IT IS
     Critical,
 }
 
 impl ExitStatus {
-    #![cfg_attr(test, allow(dead_code))]
+    /// Exit with a return code that indicates the state of the system
     pub fn exit(self) -> ! {
         use self::ExitStatus::*;
         match self {
@@ -22,8 +43,7 @@ impl ExitStatus {
         }
     }
 
-    pub fn from_str(s: &str) -> ExitStatus {
-        use ExitStatus::*;
+    /// Primarily useful to construct from argv
         match s {
             "ok" => Ok,
             "warning" => Warning,
@@ -33,6 +53,7 @@ impl ExitStatus {
         }
     }
 
+    /// The legal values for `from_str`
     pub fn str_values() -> [&'static str; 4] {
         ["ok", "warn", "critical", "unknown"]
     }
