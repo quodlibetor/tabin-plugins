a4168c1d930d7a8b073a340b6583476cf321f391
Brandon W Maister <bwm@knewton.com>
2015-09-19 15:32:56 -0400

    Create module for things relared to /proc/<pid>

--- turbine-plugins/src/procfs/mod.rs
+++ turbine-plugins/src/procfs/mod.rs
@@ -15,6 +15,8 @@ use std::num;
 
 use regex::Regex;
 
+pub mod pid;
+
 wrapped_enum!{
     #[derive(Debug)]
     /// ProcFs errors
@@ -38,7 +40,7 @@ pub struct ProcUsages<'a>(pub Usages<'a>);
 /// time period
 pub struct ProcUsage<'a> {
     /// The process we're reporting on
-    pub proc_stat: &'a ProcStat,
+    pub proc_stat: &'a pid::Stat,
     /// Percent time spent in user mode
     pub upercent: f64,
     /// Percent time spent in system mode
@@ -47,7 +49,7 @@ pub struct ProcUsage<'a> {
     pub total: f64
 }
 
-type ProcMap = HashMap<i32, ProcStat>;
+type ProcMap = HashMap<i32, pid::Stat>;
 /// All the processes that are running
 pub struct RunningProcs(pub ProcMap);
 
@@ -60,7 +62,7 @@ impl RunningProcs {
             if let Some(fname) = try!(entry).path().file_name() {
                 let fname_str = fname.to_str().unwrap();
                 if is_digit.is_match(fname_str) {
-                    let stat = try!(ProcStat::from_pid(fname_str));
+                    let stat = try!(pid::Stat::from_pid(fname_str));
                     procs.insert(stat.pid, stat);
                 }
             }
@@ -68,7 +70,7 @@ impl RunningProcs {
         Ok(RunningProcs(procs))
     }
 
-    fn iter(&self) -> hash_map::Iter<i32, ProcStat> {
+    fn iter(&self) -> hash_map::Iter<i32, pid::Stat> {
         self.0.iter()
     }
 
@@ -97,120 +99,6 @@ impl RunningProcs {
             }
         }
         ProcUsages(usages)
-    }
-}
-
-/// The status of a process
-///
-/// This represents much of the information in /proc/[pid]/stat
-#[derive(Debug)]
-pub struct ProcStat {
-    pub pid: i32,
-    pub comm: String,
-    pub state: String,
-    pub ppid: i32,
-    pub pgrp: i32,
-    pub session: i32,
-    pub tty_nr: i32,
-    pub tpgid: i32,
-    pub flags: u32,
-    pub minflt: u64,
-    pub cminflt: u64,
-    pub majflt: u64,
-    pub cmajflt: u64,
-    pub utime: u64,
-    pub stime: u64,
-    pub cutime: i64,
-    pub cstime: i64,
-    pub priority: i64,
-    pub nice: i64,
-    pub num_threads: i64,
-    pub starttime: u64,
-    pub vsize: u64,
-    pub rss: i64
-}
-
-impl ProcStat {
-    pub fn from_pid<P: fmt::Display>(pid: P) -> Result<ProcStat> {
-        let path_str = format!("/proc/{}/stat", pid);
-        let mut f = try!(File::open(&path_str));
-        let mut s = String::new();
-        try!(f.read_to_string(&mut s));
-        s.parse()
-    }
-}
-
-impl Default for ProcStat {
-    fn default() -> ProcStat {
-        ProcStat {
-            pid: 0, comm: "init".to_string(), state: "R".to_string(), ppid: 0,
-            pgrp: 0, session: 0, tty_nr: 0, tpgid: 0, flags: 0, minflt: 0,
-            cminflt: 0, majflt: 0, cmajflt: 0, utime: 0, stime: 0, cutime: 0,
-            cstime: 0, priority: 0, nice: 0, num_threads: 0, starttime: 0,
-            vsize: 0, rss: 0
-        }
-    }
-}
-
-impl FromStr for ProcStat {
-    type Err = ProcFsError;
-    /// Parse the results of /proc/[pid]/stat into a `ProcStat`
-    fn from_str(s: &str) -> Result<ProcStat> {
-        let (pid, comm, state, ppid, pgrp, session, tty_nr, tpgid, flags, minflt, cminflt, majflt,
-             cmajflt, utime, stime, cutime, cstime, priority, nice, num_threads, starttime, vsize,
-             rss) = scan_fmt!(
-            s,
-            "{d} ({[^)]}) {} {} {} {} {} {} {d} {d} {d} {d} {d} {d} {d} {} {} {} {} {} 0 {d} {d} {}",
-            i32,                    // pid
-            String,                 // comm
-            String,                 // state
-            i32,                    // ppid
-            i32,                    // pgrp
-            i32,                    // session
-            i32,                    // tty_nr
-            i32,                    // tpgid
-            u32,                    // flags
-            u64,                    // minflt
-            u64,                    // cminflt
-            u64,                    // majflt
-            u64,                    // cmajflt
-            u64,                    // utime
-            u64,                    // stime
-            i64,                    // cutime (children usertime)
-            i64,                    // cstime
-            i64,                    // priority
-            i64,                    // nice
-            i64,                    // num_threads
-            // itrealvalue (always 0)
-            u64,                    // starttime FIXME: should be long long int
-            u64,                    // vsize
-            i64                     // rss
-                );
-        Ok(ProcStat {
-            pid: pid.expect("unable to parse pid."),
-            comm: comm.expect("unable to parse comm."),
-            state: state.expect("unable to parse state."),
-            ppid: ppid.expect("unable to parse ppid."),
-            pgrp: pgrp.expect("unable to parse pgrp."),
-            session: session.expect("unable to parse session."),
-            tty_nr: tty_nr.expect("unable to parse tty_nr."),
-            tpgid: tpgid.expect("unable to parse tpgid."),
-            flags: flags.expect("unable to parse flags."),
-            minflt: minflt.expect("unable to parse minflt."),
-            cminflt: cminflt.expect("unable to parse cminflt."),
-            majflt: majflt.expect("unable to parse majflt."),
-            cmajflt: cmajflt.expect("unable to parse cmajflt."),
-            utime: utime.expect("unable to parse utime."),
-            stime: stime.expect("unable to parse stime."),
-            cutime: cutime.expect("unable to parse cutime."),
-            cstime: cstime.expect("unable to parse cstime."),
-            priority: priority.expect("unable to parse priority."),
-            nice: nice.expect("unable to parse nice."),
-            num_threads: num_threads.expect("unable to parse num_threads."),
-            starttime: starttime.expect("unable to parse starttime."),
-            vsize: vsize.expect("unable to parse vsize."),
-            rss: rss.expect("unable to parse rss.")
-        })
     }
 }
 
@@ -632,7 +520,8 @@ impl fmt::Display for LoadAvg {
 
 #[cfg(test)]
 mod unit {
-    use super::{Calculations, ProcStat, MemInfo, LoadAvg};
+    use super::{Calculations, MemInfo, LoadAvg};
+    use super::pid;
     use std::str::FromStr;
 
     #[test]
@@ -661,7 +550,7 @@ btime 143
     #[test]
     fn can_parse_stat_for_process() {
         let stat = "1 (init) S 0 1 1 0 -1 4219136 40326 5752369 36 3370 16 41 25846 8061 20 0 1 0 5 34381824 610 18446744073709551615 1 1 0 0 0 0 0 4096 536962595 18446744073709551615 0 0 17 0 0 0 7 0 0 0 0 0 0 0 0 0 0";
-        stat.parse::<ProcStat>().unwrap();
+        stat.parse::<pid::Stat>().unwrap();
     }
 
     #[test]
---
+++ turbine-plugins/src/procfs/pid.rs
@@ -0,0 +1,123 @@
+//! Data structures related to the /proc/<pid>/* files
+
+use std::fmt;
+use std::fs::File;
+use std::io::Read;
+use std::str::FromStr;
+
+use super::ProcFsError;
+use super::Result;
+
+/// The status of a process
+///
+/// This represents much of the information in /proc/[pid]/stat
+#[derive(Debug)]
+pub struct Stat {
+    pub pid: i32,
+    pub comm: String,
+    pub state: String,
+    pub ppid: i32,
+    pub pgrp: i32,
+    pub session: i32,
+    pub tty_nr: i32,
+    pub tpgid: i32,
+    pub flags: u32,
+    pub minflt: u64,
+    pub cminflt: u64,
+    pub majflt: u64,
+    pub cmajflt: u64,
+    pub utime: u64,
+    pub stime: u64,
+    pub cutime: i64,
+    pub cstime: i64,
+    pub priority: i64,
+    pub nice: i64,
+    pub num_threads: i64,
+    pub starttime: u64,
+    pub vsize: u64,
+    pub rss: i64
+}
+
+impl Stat {
+    pub fn from_pid<P: fmt::Display>(pid: P) -> Result<Stat> {
+        let path_str = format!("/proc/{}/stat", pid);
+        let mut f = try!(File::open(&path_str));
+        let mut s = String::new();
+        try!(f.read_to_string(&mut s));
+        s.parse()
+    }
+}
+
+impl Default for Stat {
+    fn default() -> Stat {
+        Stat {
+            pid: 0, comm: "init".to_string(), state: "R".to_string(), ppid: 0,
+            pgrp: 0, session: 0, tty_nr: 0, tpgid: 0, flags: 0, minflt: 0,
+            cminflt: 0, majflt: 0, cmajflt: 0, utime: 0, stime: 0, cutime: 0,
+            cstime: 0, priority: 0, nice: 0, num_threads: 0, starttime: 0,
+            vsize: 0, rss: 0
+        }
+    }
+}
+
+impl FromStr for Stat {
+    type Err = ProcFsError;
+    /// Parse the results of /proc/[pid]/stat into a `Stat`
+    fn from_str(s: &str) -> Result<Stat> {
+        let (pid, comm, state, ppid, pgrp, session, tty_nr, tpgid, flags, minflt, cminflt, majflt,
+             cmajflt, utime, stime, cutime, cstime, priority, nice, num_threads, starttime, vsize,
+             rss) = scan_fmt!(
+            s,
+            "{d} ({[^)]}) {} {} {} {} {} {} {d} {d} {d} {d} {d} {d} {d} {} {} {} {} {} 0 {d} {d} {}",
+            i32,                    // pid
+            String,                 // comm
+            String,                 // state
+            i32,                    // ppid
+            i32,                    // pgrp
+            i32,                    // session
+            i32,                    // tty_nr
+            i32,                    // tpgid
+            u32,                    // flags
+            u64,                    // minflt
+            u64,                    // cminflt
+            u64,                    // majflt
+            u64,                    // cmajflt
+            u64,                    // utime
+            u64,                    // stime
+            i64,                    // cutime (children usertime)
+            i64,                    // cstime
+            i64,                    // priority
+            i64,                    // nice
+            i64,                    // num_threads
+            // itrealvalue (always 0)
+            u64,                    // starttime FIXME: should be long long int
+            u64,                    // vsize
+            i64                     // rss
+                );
+        Ok(Stat {
+            pid: pid.expect("unable to parse pid."),
+            comm: comm.expect("unable to parse comm."),
+            state: state.expect("unable to parse state."),
+            ppid: ppid.expect("unable to parse ppid."),
+            pgrp: pgrp.expect("unable to parse pgrp."),
+            session: session.expect("unable to parse session."),
+            tty_nr: tty_nr.expect("unable to parse tty_nr."),
+            tpgid: tpgid.expect("unable to parse tpgid."),
+            flags: flags.expect("unable to parse flags."),
+            minflt: minflt.expect("unable to parse minflt."),
+            cminflt: cminflt.expect("unable to parse cminflt."),
+            majflt: majflt.expect("unable to parse majflt."),
+            cmajflt: cmajflt.expect("unable to parse cmajflt."),
+            utime: utime.expect("unable to parse utime."),
+            stime: stime.expect("unable to parse stime."),
+            cutime: cutime.expect("unable to parse cutime."),
+            cstime: cstime.expect("unable to parse cstime."),
+            priority: priority.expect("unable to parse priority."),
+            nice: nice.expect("unable to parse nice."),
+            num_threads: num_threads.expect("unable to parse num_threads."),
+            starttime: starttime.expect("unable to parse starttime."),
+            vsize: vsize.expect("unable to parse vsize."),
+            rss: rss.expect("unable to parse rss.")
+        })
+    }
+}
