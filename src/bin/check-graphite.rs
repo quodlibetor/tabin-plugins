23176e41a5d8bac6c7878fb493cecd29a99913d6
Brandon W Maister <quodlibetor@gmail.com>
2017-03-07 17:43:20 -0500

    cg: Add a --window-start parameter
    This allows you to see how your query performs when looking at the past.

--- src/bin/check-graphite.rs
+++ src/bin/check-graphite.rs
@@ -240,13 +240,15 @@ impl fmt::Display for GraphiteError {
 fn get_graphite(url: &str,
                 target: &str,
                 window: i64,
+                start_at: i64,
                 print_url: bool,
                 graphite_error: &Status)
                 -> Result<GraphiteResponse, GraphiteError> {
-    let full_path = format!("{}/render?target={}&format=json&from=-{}min",
+    let full_path = format!("{}/render?target={}&format=json&from=-{}min&until=-{}min",
                             url,
                             target,
-                            window);
+                            window,
+                            start_at);
     let c = hyper::Client::new();
     if print_url {
         println!("INFO: querying {}", full_path);
@@ -277,7 +279,7 @@ fn get_graphite(url: &str,
 
 struct GraphiteResponse {
     result: Json,
-    url: url::Url,
+    url: hyper::Url,
 }
 
 /// Load data from graphite
@@ -286,6 +288,7 @@ struct GraphiteResponse {
 fn fetch_data(url: &str,
               target: &str,
               window: i64,
+              start_at: i64,
               retries: u8,
               graphite_error: &Status,
               print_url: bool)
@@ -293,7 +296,7 @@ fn fetch_data(url: &str,
     let mut attempts = 0;
     let mut retry_sleep = 2000;
     loop {
-        match get_graphite(url, target, window, print_url, graphite_error) {
+        match get_graphite(url, target, window, start_at, print_url, graphite_error) {
             Ok(s) => {
                 return Ok(s);
             }
@@ -545,6 +548,7 @@ struct Args {
     path: String,
     assertions: Vec<Assertion>,
     window: i64,
+    start_at: i64,
     retries: u8,
     graphite_error: Status,
     no_data: Status,
@@ -570,91 +574,67 @@ fn parse_args() -> Args {
                    .author("Brandon W Maister <quodlibetor@gmail.com>")
                    .about("Query graphite and exit based on predicates")
                    .args_from_usage("<URL>                 'The domain to query graphite. Must \
-                                     include scheme (http/s)'
-             <PATH>                \
-                                     'The graphite path to query. For example: \
-                                     \"collectd.*.cpu\"'
-             <ASSERTION>...        'The \
-                                     assertion to make against the PATH. See Below.'
-             \
+                                         include scheme (http/s)'
+                                     <PATH> 'The graphite path to query. For example: \
+                                         \"collectd.*.cpu\"'
+                                     <ASSERTION>...        'The assertion to make against the PATH. \
+                                         See Below.'
                                      -w --window=[MINUTES] 'How many minutes of data to test. \
-                                     Default 10.'
-             --retries=[COUNT]     'How many \
-                                     times to retry reaching graphite. Default 4.
-             \
-                                     --print-url           'Unconditionally print the graphite \
-                                     url queried'
-             --verify-assertions   'Just check \
-                                     assertion syntax, do not query urls'")
+                                         Default 10.'
+                                     --window-start=[MINUTES_IN_PAST] 'How far back to start the window. \
+                                         Default is now.'
+                                    --retries=[COUNT]     'How many times to retry reaching graphite. \
+                                        Default 4.'
+                                    --print-url           'Unconditionally print the graphite \
+                                        url queried'
+                                    --verify-assertions   'Just check assertion syntax, do not \
+                                        query urls'")
                    .arg(clap::Arg::with_name("NO_DATA_STATUS")
                             .long("--no-data")
-                            .help("What to do with no data.
-                              \
-                                   Choices: ok, warn, critical, unknown.
-                              \
-                                   This is the value to use for the assertion 'if all values \
+                            .help("What to do with no data. Choices: ok, warn, critical, unknown.
+                                   This is the value to use for the assertion 'if all values 
                                    are null'
-                              Default: warn.")
+                                  Default: warn.")
                             .takes_value(true)
                             .possible_values(&allowed_no_data))
                    .arg(clap::Arg::with_name("GRAPHITE_ERROR_STATUS")
                             .long("--graphite-error")
                             .help("What to do with no data.
-                              \
                                    Choices: ok, warn, critical, unknown.
-                              \
                                    What to say if graphite returns a 500 or invalid JSON
-                              \
                                    Default: unknown.")
                             .takes_value(true)
                             .possible_values(&allowed_no_data))
                    .after_help(&format!("About Assertions:
 
-    Assertions look like 'critical \
-                                         if any point in any series is > 5'.
+    Assertions look like 'critical if any point in any series is > 5'.
 
-    They describe \
-                                         what you care about in your graphite data. The \
-                                         structure of
+    They describe what you care about in your graphite data. The structure of
     an assertion is as follows:
 
-        \
-                                         <errorkind> if <point spec> [in <series spec>] is|are \
-                                         [not] <operator> <threshold>
+        <errorkind> if <point spec> [in <series spec>] is|are [not] <operator> <threshold>
 
     Where:
 
-        - \
-                                         `errorkind` is either `critical` or `warning`
-        \
-                                         - `point spec` can be one of:
-            - `any \
-                                         point`
-            - `all points`
-            - `at \
-                                         least <N>% of points`
-            - `most recent \
-                                         point`
-        - `series spec` (optional) can be one \
-                                         of:
+        - `errorkind` is either `critical` or `warning`
+        - `point spec` can be one of:
+        - `any point`
+        - `all points`
+        - `at least <N>% of points`
+        - `most recent point`
+        - `series spec` (optional) can be one of:
             - `any series`
-            - `all \
-                                         series`
+            - `all series`
             - `at least <N>% of series`
-        \
-                                         - `not` is optional, and inverts the following \
-                                         operator
-        - `operator` is one of: `==` `!=` `<` \
-                                         `>` `<=` `>=`
-        - `threshold` is a \
-                                         floating-point value (e.g. 100, 78.0)
+            - `not` is optional, and inverts the following operator
+        - `operator` is one of: `==` `!=` `<` `>` `<=` `>=`
+        - `threshold` is a floating-point value (e.g. 100, 78.0)
 
-    Here are \
-                                         some example assertions:
+    Here are some example assertions:
 
         - `{}`\n",
-                                        ASSERTION_EXAMPLES.join("`\n        - `")))
-                   .get_matches();
+        ASSERTION_EXAMPLES.join("`\n        - `")))
+        .get_matches();
 
     let assertions = args.values_of("ASSERTION")
                          .unwrap()
@@ -674,12 +654,14 @@ fn parse_args() -> Args {
         Status::Ok.exit();
     }
 
-
+    let start_offset = value_t!(args.value_of("MINUTES_IN_PAST"), i64).unwrap_or(0);
+    let window = value_t!(args.value_of("MINUTES"), i64).unwrap_or(10);
     Args {
         url: args.value_of("URL").unwrap().to_owned(),
         path: args.value_of("PATH").unwrap().to_owned(),
         assertions: assertions,
-        window: value_t!(args.value_of("MINUTES"), i64).unwrap_or(10),
+        window: start_offset + window,
+        start_at: start_offset,
         retries: value_t!(args.value_of("COUNT"), u8).unwrap_or(4),
         graphite_error: Status::from_str(args.value_of("GRAPHITE_ERROR_STATUS")
                                              .unwrap_or("unknown"))
@@ -960,6 +942,7 @@ fn main() {
     let data = match fetch_data(&args.url,
                                 &args.path,
                                 args.window,
+                                args.start_at,
                                 args.retries,
                                 &args.graphite_error,
                                 args.print_url) {
